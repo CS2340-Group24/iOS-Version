aa37f3ae90e5177d39d8e2017c43b3a2f4db189f
Amiel Berchenko <aberchenko3@lwcimac09.tss.gatech.edu>
2018-03-12 18:03:15 -0400

    Trying to upload Firebase SDK.

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Shelter Finder' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Shelter Finder
+  pod ‘Firebase/Core’
+  pod 'Firebase/Database'
+
+  target 'Shelter FinderTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Shelter FinderUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Shelter Finder/ListViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ListViewController.swift
+//  Shelter Finder
+//
+//  Created by Berchenko, Amiel D on 3/9/18.
+//  Copyright © 2018 Berchenko, Amiel D. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
